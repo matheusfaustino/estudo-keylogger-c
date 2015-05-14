ac3190c64f89be8b6d54fefa58d55e3b5a4c09a0
Matheus Faustino <matheus.faustino@arcasolutions.com>
2015-05-14 13:47:47 -0300

    Changes tab to space

--- signal.c
+++ signal.c
@@ -9,10 +9,10 @@
  */
 void signalDie(int signum)
 {
-	printf("Sinal pego #%d, saindo...\n", signum);
+    printf("Sinal pego #%d, saindo...\n", signum);
 
-	// tem o exit pq senao o programa nao finaliza msm com o ctrl+c
-	exit(1);
+    // tem o exit pq senao o programa nao finaliza msm com o ctrl+c
+    exit(1);
 }
 
 /**
@@ -21,18 +21,18 @@ void signalDie(int signum)
  */
 void signalTerm(int signum)
 {
-	printf("Terminar requisicao: #%d\n", signum);
-	exit(1);
+    printf("Terminar requisicao: #%d\n", signum);
+    exit(1);
 }
 
 int main(int argc, char const *argv[])
 {
-	signal(SIGINT, signalDie);
-	signal(SIGTERM, signalTerm);
+    signal(SIGINT, signalDie);
+    signal(SIGTERM, signalTerm);
 
-	while (1) {
-		printf("Going to sleep for a second... \n");
-		sleep(1);
-	}
-	return 0;
+    while (1) {
+        printf("Going to sleep for a second... \n");
+        sleep(1);
+    }
+    return 0;
 }
